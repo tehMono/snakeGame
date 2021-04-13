0ed4db5cbac35b13eeb87da9153d1d01b8a62510
Andrew <tehMono2@gmail.com>
2021-04-13 21:00:44 +0500

    class Point

--- Game-snake/Game-snake/Program.cs
+++ Game-snake/Game-snake/Program.cs
@@ -10,27 +10,23 @@ namespace Game_snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
             Console.ReadKey();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 3;
+            p2.y = 7;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadKey();
         }
 
-        static void Draw(int x, int y,char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
-        }
+        
     }
 }
